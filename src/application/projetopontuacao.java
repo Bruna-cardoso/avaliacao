1b111c7ccfe58a9bfde3c583ac372098b77edb3c
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-15 19:37:28 -0300

    Leitura do volume

--- src/application/projetopontuacao.java
+++ src/application/projetopontuacao.java
@@ -11,11 +11,22 @@ public class projetopontuacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int qntcompras;
+		double ticket;
+		
 		// Cabecalho
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("---------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		
+		// Leitura do volume
+		
+		System.out.println("Quantas compras o cliente fez no último ano?");
+		qntcompras = sc.nextInt();
+		System.out.println("Qual o ticket médio?");
+		ticket = sc.nextDouble();
 		System.out.println();
 		
 		
