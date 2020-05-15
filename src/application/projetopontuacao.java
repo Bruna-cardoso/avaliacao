f4928a92f03f9491d0431537a43e9463ad09f7bb
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-15 19:31:07 -0300

    Cabecalho

--- src/application/projetopontuacao.java
+++ src/application/projetopontuacao.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class projetopontuacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		// Cabecalho
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("---------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		
+		
+		sc.close();
 	}
 
 }
